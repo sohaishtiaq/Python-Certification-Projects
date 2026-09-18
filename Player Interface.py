b961a4e5a9dcf476a05d72254ac1fd17e9980d8f
Soha Ishtiaq <sohaishtiaq15@gmail.com>
2026-09-18 16:07:27 +0500

    Create Player Interface.py

---
+++ Player Interface.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+import random
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
+    
+    def make_move(self):
+        move = random.choice(self.moves)
+        new_position = (self.position[0] + move[0], self.position[1] + move[1])
+        self.position = new_position
+        self.path.append(self.position)
+        return self.position
+
+    @abstractmethod
+    def level_up(self):
+        pass
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [(1,0), (-1,0), (0,1), (0,-1)]
+
+    def level_up(self):
+        new_moves = [(1,1), (1,-1), (-1,1), (-1,-1)]
+        self.moves += new_moves
