c492bb962e2e7c88719734d5f4817d25355ad0cb
Soha Ishtiaq <sohaishtiaq15@gmail.com>
2026-09-21 18:20:28 +0500

    Polygon Area Calculator.py

--- Polygon Area Calculator.py
+++ Polygon Area Calculator.py
@@ -1,5 +1,5 @@
 class Rectangle:
-    def __init__(self,height,width):
+    def __init__(self,width, height):
         self._width = width
         self._height = height
 
@@ -11,8 +11,38 @@ class Rectangle:
     def height(self):
         return self._height
 
+    def set_width(self,new_width):
+        self._width = new_width
+
+    def set_height(self,new_height):
+        self._height = new_height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        image = ''
+        for i in range( self.height):
+            image += '*' * self.width
+            image += '\n' 
+        return image
+
+    def get_amount_inside(self,shape):
+        fit_width = self.width // shape.width
+        fit_height = self.height // shape.height
+        return fit_width * fit_height      
+
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
+    
 class Square(Rectangle):
     def __init__(self, side):
         super().__init__(side,side)
@@ -20,8 +50,26 @@ class Square(Rectangle):
    
     @property
     def side(self):
-        return self._side
+        return self._width
+
+    def set_side(self, new_side):
+        self._width = new_side
+        self._height = new_side
+
+    def set_width(self, new_side):
+        self.set_side(new_side)
+
+    def set_height(self, new_side):
+        self.set_side(new_side)
+
+    def get_picture(self):
+        if self.side > 50:
+            return 'Too big for picture.'
+        image = ''
+        for i in range(self.side):
+            image += '*' * self.side
+            image += '\n' 
+        return image
 
     def __str__(self):
         return f"Square(side={self.side})"
-    
